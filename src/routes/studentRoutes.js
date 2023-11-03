50ead425c2dbeb0d7863430f5b461c637ccb3c09
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-11-03 14:29:51 +0300

    Update studentRoutes.js

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getStudents , addStudent , deleteStudent, updateStudent } = require('../controllers/studentController');
+const studentController = require('../controllers/studentController');
 
-router.get('/students', async (req, res) => {
-    getStudents(req, res);
-  });
+router.get('/students', studentController.getStudents);
 
-router.post('/addStudent', async (req, res) => {
-    addStudent(req, res);
-});
+router.post('/addStudent', studentController.addStudent);
 
-router.delete('/deleteStudent/:studentId', async (req, res) => {
-    deleteStudent(req, res);
-});
+router.delete('/deleteStudent/:studentId', studentController.deleteStudent);
 
-
-router.put('/updateStudent/:studentId', async (req, res) => {
-    updateStudent(req, res);
-  });
+router.put('/updateStudent/:studentId', studentController.updateStudent);
 
 module.exports = router;
